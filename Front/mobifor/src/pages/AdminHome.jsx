00be8d69526eb3c548550e8934bfe269d27b8e4c
Mateus Lopes <mlopescpu@gmail.com>
2026-05-27 11:18:55 -0300

    Exibindo nome do admin

--- Front/mobifor/src/pages/AdminHome.jsx
+++ Front/mobifor/src/pages/AdminHome.jsx
@@ -2,12 +2,15 @@ import ParkingMap from "../components/map/ParkingMap"
 import AdminSection from "../components/admin/AdminSection"
 import ReservaValidacaoSection from '../components/admin/ReservaValidacaoSection'
 import UniBusSection from '../components/admin/UniBusSection'
+import { buscarUsuario } from "../utils/authStorage"
 
 export default function AdminHome() {
+  const usuario = buscarUsuario()
+
   return (
     <main className="min-h-[calc(100vh-128px)] bg-blue-50 p-6">
       <h1 className="text-2xl font-bold text-gray-800 mb-6 ml-6 py-4">
-        Painel Administrativo
+        Olá, {usuario?.nome || "administrador"}!
       </h1>
 
       <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-4">
@@ -21,4 +24,3 @@ export default function AdminHome() {
     </main>
   )
 }
-
